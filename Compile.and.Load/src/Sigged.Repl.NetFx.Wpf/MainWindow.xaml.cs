f491650cfb8fada6ec9a8bff347dcd0acfc27fc0
sigged <sderdeyn@gmail.com>
2019-04-07 00:34:54 +0200

    added REPL project in wpf with mvvm

--- Compile.and.Load/src/Sigged.Repl.NetFx.Wpf/MainWindow.xaml.cs
+++ Compile.and.Load/src/Sigged.Repl.NetFx.Wpf/MainWindow.xaml.cs
@@ -1,7 +1,14 @@
-﻿using System;
+﻿using AurelienRibon.Ui.SyntaxHighlightBox;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Resources;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,7 +19,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
 namespace Sigged.Repl.NetFx.Wpf
 {
     /// <summary>
@@ -23,6 +32,73 @@ namespace Sigged.Repl.NetFx.Wpf
         public MainWindow()
         {
             InitializeComponent();
+            InitializeHighlighters();
+            txtSource.CurrentHighlighter = HighlighterManager.Instance.Highlighters["CSharp"];
+
+            DataContext = new MainWindowsViewModel();
+        }
+
+        private void InitializeHighlighters()
+        {
+            var xsd = Application.GetResourceStream(new Uri("pack://application:,,,/AurelienRibon.Ui.SyntaxHighlightBox;component/resources/syntax.xsd"));
+            var schemaStream = xsd.Stream;
+            XmlSchema schema = XmlSchema.Read(schemaStream, (s, e) =>
+            {
+                Debug.WriteLine("Xml schema validation error : " + e.Message);
+            });
+
+            XmlReaderSettings readerSettings = new XmlReaderSettings();
+            readerSettings.Schemas.Add(schema);
+            readerSettings.ValidationType = ValidationType.Schema;
+
+            foreach (var res in GetResources("resources/(.+?)[.]xml"))
+            {
+                XDocument xmldoc = null;
+                try
+                {
+                    XmlReader reader = XmlReader.Create(res.Value, readerSettings);
+                    xmldoc = XDocument.Load(reader);
+                }
+                catch (XmlSchemaValidationException ex)
+                {
+                    Debug.WriteLine("Xml validation error at line " + ex.LineNumber + " for " + res.Key + " :");
+                    Debug.WriteLine("Warning : if you cannot find the issue in the xml file, verify the xsd file.");
+                    Debug.WriteLine(ex.Message);
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    return;
+                }
+
+                XElement root = xmldoc.Root;
+                String name = root.Attribute("name").Value.Trim();
+                HighlighterManager.Instance.Highlighters.Add(name, new XmlHighlighter(root));
+            }
+
+        }
+
+        /// <summary>
+		/// Returns a dictionary of the assembly resources (not embedded).
+		/// Uses a regex filter for the resource paths.
+		/// </summary>
+		private IDictionary<string, UnmanagedMemoryStream> GetResources(string filter)
+        {
+            var asm = Assembly.GetCallingAssembly();
+            string resName = asm.GetName().Name + ".g.resources";
+            Stream manifestStream = asm.GetManifestResourceStream(resName);
+            ResourceReader reader = new ResourceReader(manifestStream);
+
+            IDictionary<string, UnmanagedMemoryStream> ret = new Dictionary<string, UnmanagedMemoryStream>();
+            foreach (DictionaryEntry res in reader)
+            {
+                string path = (string)res.Key;
+                UnmanagedMemoryStream stream = (UnmanagedMemoryStream)res.Value;
+                if (Regex.IsMatch(path, filter))
+                    ret.Add(path, stream);
+            }
+            return ret;
         }
     }
 }
---
+++ Compile.and.Load/src/Sigged.Repl.NetFx.Wpf/MainWindowsViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace Sigged.Repl.NetFx.Wpf
+{
+    public class MainWindowsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string sourceCode;
+        public string SourceCode
+        {
+            get { return sourceCode; }
+            set {
+                sourceCode = value;
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(Clear));
+            }
+        }
+
+        public ICommand Clear => new RelayCommand(
+            () => {
+                SourceCode = "";
+            }, 
+            () => {
+                return SourceCode?.Length > 0;
+            }
+        );
+
+        public ICommand Build => new RelayCommand(() => {
+
+        });
+
+        public ICommand BuildAndRun => new RelayCommand(() => {
+
+        });
+
+    }
+}
