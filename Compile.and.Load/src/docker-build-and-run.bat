1d8b74c197dd786afa1c15b772b18658d1374829
sigged <sderdeyn@gmail.com>
2019-04-16 20:35:44 +0200

    Update docker scripts to reflect project name changes

--- Compile.and.Load/src/docker-build-and-run.bat
+++ Compile.and.Load/src/docker-build-and-run.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 ECHO Building Dockerimage
-docker build -f Dockerfile -t sigged/insecure-repl .
+docker build -f Dockerfile -t sigged/insecure-csc .
 
 REM this app MUST be run on the same forward-facing port, or SignalRClientService won't work
-docker run -it --rm -p 80:80 --name insecure-repl sigged/insecure-repl
+docker run -it --rm -p 80:80 --name insecure-csc sigged/insecure-csc
 PAUSE
--- Compile.and.Load/src/docker-build-and-run.sh
+++ Compile.and.Load/src/docker-build-and-run.sh
@@ -1,6 +1,6 @@
 echo Building Dockerimage 
 
-docker build -f Dockerfile -t sigged/insecure-repl .
+docker build -f Dockerfile -t sigged/insecure-csc .
 
 #this app MUST be run on the same forward-facing port, or SignalRClientService won't work
-docker run -it --rm -p 80:80 --name insecure-repl sigged/insecure-repl
+docker run -it --rm -p 80:80 --name insecure-csc sigged/insecure-csc
